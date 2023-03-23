268f109e52836b41eae79b5bbf5d9d22be31fd95
GhostServant <nik7nuans@inbox.ru>
2023-03-23 19:31:29 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Button from './components/Button/button'
+import Login from './components/Login/login'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 function App() {
   return (
     <div className="App">
-      <Button/>      
+      <GoogleOAuthProvider clientId="103408156486-oi1sggasovhdi35bc4av65rrlt2rteus.apps.googleusercontent.com"><Login/></GoogleOAuthProvider>;   
     </div>
   );
 }
