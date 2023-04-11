e8ef68c0930f41ba34a68324941fed81e6ad75d6
oribon <guardnight62@gmail.com>
2023-04-11 20:40:37 +0300

    add pagination

---
+++ src/pages/Paggination.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Paggination = ({bookPerPage, totalBook, paginate}) => {
+    const pageNumbers = []
+    
+    for(let i=1; i <= Math.ceil(totalBook/bookPerPage); i++){
+        pageNumbers.push(i)
+    }
+
+    return(
+        <div>
+            <ul className = "pagination">
+                {
+                    pageNumbers.map(number => (
+                        <li className="page-item" key={number}>
+                            <a href="!#" className="pagelink" onClick={() => paginate()}>
+                                {number}
+                            </a>
+
+                        </li>
+                    ))
+                }   
+            </ul>
+        </div>
+    )
+}
+
+export default Paggination;
--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -2,28 +2,62 @@ import './catalog.scss'
 import { useState,useEffect } from 'react'
 import axios from 'axios';
 import Card from '../../components/Card/Card'
+import Paggination from '../Paggination'
 
 
 function Catalog(){
-  const [info,setInfo] = useState([]);
-  useEffect(()=>{
-    const getCardInfo = async()=>{
-      const request = await axios.get('http://localhost:8000/');
-      try{
-        setInfo(request.data);
-      }catch(error){
-        console.error(error);
-      }
+  const [book, setBook] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentpage, setCurrentpage] = useState(1);
+  const [bookperpage] = useState(10);
+
+  useEffect (() =>{
+    const getBook = async () => {
+      setLoading(true)
+      const res = await axios.get('http://localhost:8000')
+      setBook(res.data)
+      setLoading(false)
     }
-    //getCardInfo();
-  },[])
+
+
+    getBook()
+  }, [])
+
+  const lastBookIndex = currentpage * bookperpage
+  const firstbookIndex = lastBookIndex - bookperpage
+  const currentBook = book.slice(firstbookIndex, lastBookIndex)
+
+  const paginate = pageNumber => setCurrentpage(pageNumber)
+  const nextPage = () => setCurrentpage(prev => prev+1)
+  const prevPage = () => setCurrentpage(prev => prev-1)
+
+  if(loading)
+  {
+    return(
+    <div className='catalog__inner'>
+      <div className='catalog__content'>
+        <div className="catalog__search">
+          <div className="catalog__filtration">
+           Ждесь будет много гемороя
+            </div>
+          <select className="catalog__sort">
+            <option>По новизне</option>
+            <option>По популярности</option>
+            <option>По алфавиту</option>
+          </select>
+        </div>
+        <h2>Loading...</h2>
+      </div>
+    </div> 
+    )
+  }
   return(
     <div className='catalog__inner'>
       <div className='catalog__content'>
         <div className="catalog__search">
-          <button className="catalog__filtration">
-            Фильтрация
-          </button>
+          <div className="catalog__filtration">
+           Ждесь будет много гемороя
+            </div>
           <select className="catalog__sort">
             <option>По новизне</option>
             <option>По популярности</option>
@@ -31,10 +65,18 @@ function Catalog(){
           </select>
         </div>
         {
-          info.map((obj)=>
+          currentBook.map((obj)=>
             <Card key={obj.id} titleLink={obj.title.split(' ').join('')} author={obj.author} title={obj.title} genre={obj.genre}/>
           )
         }
+        <Paggination
+          bookPerPage = {bookperpage}
+          totalBook = {book.length}
+          paginate = {paginate}
+        />
+
+        <button className="btn btn-primary" onClick={prevPage}>Prev Page</button>
+        <button className="btn btn-primary" onClick={nextPage}>Next Page</button>
       </div>
     </div> 
   )
