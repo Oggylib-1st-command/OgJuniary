38facbe228fe519e2e8b635755d07305806ce67f
GhostServant <nik7nuans@inbox.ru>
2023-04-21 01:02:08 +0300

    теперь можно добавлять фото и собирать данные по полям

---
+++ components/admin/adminCatalog/Catalog.jsx
@@ -0,0 +1,7 @@
+import "./Catalog.scss";
+
+function Catalog() {
+  return <div className="admin-catalog__inner"></div>;
+}
+
+export default Catalog;
---
+++ pages/takenBook/TakenBook.jsx
@@ -0,0 +1,8 @@
+import "./TakenBook.scss";
+export const TakenBook = () => {
+  return (
+    <div>
+      <p>Взятые книги</p>
+    </div>
+  );
+};
